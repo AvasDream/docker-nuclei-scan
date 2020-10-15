1a1d3f8855a80ab9d7210c9743168fafd89081ce
vgrimmeisen <vincent.grimmeisen@redguard.ch>
2020-10-15 20:37:29 +0200

    Unique debug file

--- main.sh
+++ main.sh
@@ -8,7 +8,7 @@ function notify {
 }
 
 function nuclei-exec {
-    nuclei -nC -l "$inputfile" -t "$toolsDir"'/nuclei/nuclei-templates/' -timeout 3 -severity low,medium,high -o "/data/nuclei-$DOMAIN.txt" &> /data/debug-nuclei.txt
+    nuclei -nC -l "$inputfile" -t "$toolsDir"'/nuclei/nuclei-templates/' -timeout 3 -severity low,medium,high -o "/data/nuclei-$DOMAIN.txt" &> /data/debug-nuclei-$DOMAIN.txt
     nuclei_found=$(cat "/data/nuclei-$DOMAIN.txt" | cut -d" " -f1 | sort -u )
     message=$(echo -e "$DOMAIN:\n$nuclei_found")
     notify "$message"
